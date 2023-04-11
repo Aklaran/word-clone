217c8099251d4118871f772190ea03477fc868a4
aklaran <botbunk@gmail.com>
2023-04-11 14:28:24 -0400

    creates guess input

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+import GuessInput from "../GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +11,22 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+
+  function validateGuess() {
+    console.info({ guess });
+    setGuess("");
+  }
+
+  return (
+    <>
+      <GuessInput
+        guess={guess}
+        setGuess={setGuess}
+        validateGuess={validateGuess}
+      />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function GuessInput({ guess, setGuess, validateGuess }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    validateGuess();
+  }
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => handleSubmit(event)}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        value={guess}
+        id="guess-input"
+        type="text"
+        pattern="[A-Z]{5}"
+        title="5 letter guess"
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
